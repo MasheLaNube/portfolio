d1296851ea2d5126bda6f6f79a1f9abdc91466f5
Marcela López <111093992+MasheLaNube@users.noreply.github.com>
2025-12-13 12:01:37 -0300

    Delete JS directory

--- JS/script2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('form').on('submit', function (e) {
-        e.preventDefault();
-        alert('Gracias por tu mensaje, trataré de estar en contacto pronto.');
-        this.reset();
-    });
-});
