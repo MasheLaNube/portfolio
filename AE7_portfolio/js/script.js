688913a625bdf1530c8d8be08090d6afda2cf36e
Marcela López <111093992+MasheLaNube@users.noreply.github.com>
2025-12-13 12:01:52 -0300

    Delete AE7_portfolio directory

--- AE7_portfolio/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $('.titulo-virus').on('click', function() {
-        $(this).toggleClass('abierto');
-        $(this).next('.contenido-virus').slideToggle(300);
-    });
-});
-
