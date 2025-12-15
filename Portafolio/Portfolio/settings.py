b4c30a0f3b6481316d56b43f7ef737030de48245
mashelanube <marcelalopezulloa@gmail.com>
2025-12-15 16:35:03 -0300

    Add Cloudinary media storage

--- Portafolio/Portfolio/settings.py
+++ Portafolio/Portfolio/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'presentacion',
     'artesanias',
     'proyectos',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +117,8 @@ if DEBUG:
 else:
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
